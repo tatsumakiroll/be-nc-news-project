472c528baeae31a15b14681778796ff2088f671f
tatsumakiroll <paul.shoreman@outlook.com>
2024-04-15 18:15:48 +0100

    "task 3 done and tested"

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
-const { getAllTopics } = require('./controllers/topics.controllers')
+const { getApi, getAllTopics } = require('./controllers/topics.controllers')
 
-app.get("/api/topics", getAllTopics);
+app.use(express.json());
 
+app.get("/api", getApi)
+
+app.get("/api/topics", getAllTopics)
+
+app.all('*', (req, res, next)=>{
+        res.status(404).send({message: 'not found'}) 
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: "internal server error" })
+  })
+  
+ 
 
 module.exports = app
--- models/topics.models.js
+++ models/topics.models.js
@@ -1,5 +1,12 @@
 const db = require('../db/connection')
+const fs = require('fs/promises')
 
+exports.selectApi = () => {
+    return fs.readFile('./endpoints.json', 'utf-8').then((response) => {
+        const result = JSON.parse(response)
+        return result;
+    })
+}
 exports.selectAllTopics = () => {
     return db.query(`
     SELECT * 
