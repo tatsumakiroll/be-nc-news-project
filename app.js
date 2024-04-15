86d5b3ef2220ed0fc73cc68a8abbcaf0ff1f4e2b
tatsumakiroll <paul.shoreman@outlook.com>
2024-04-15 21:05:34 +0100

    added get articlesById

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { getApi, getAllTopics } = require('./controllers/topics.controllers')
+const { getApi, getAllTopics, getArticleById } = require('./controllers/topics.controllers')
 
 app.use(express.json());
 
@@ -8,14 +8,28 @@ app.get("/api", getApi)
 
 app.get("/api/topics", getAllTopics)
 
-app.all('*', (req, res, next)=>{
-        res.status(404).send({message: 'not found'}) 
+app.get("/api/articles/:article_id", getArticleById)
+
+app.all('*', (req, res, next) => {
+    res.status(404).send({ message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-    res.status(500).send({ message: "internal server error" })
-  })
-  
- 
+    if (err.status && err.message) {
+        res.status(err.status).send({ message: err.message })
+    } else next(err)
+})
+
+app.use((err, req, res, next) => {
+    if (err.code === '22P02' || err.code === '23502') {
+        res.status(400).send({ message: 'Bad request' })
+    } else next(err)
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: "Internal Server Error" })
+})
+
+
 
 module.exports = app
