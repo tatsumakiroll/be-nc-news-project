d940f81864c8acdf276c2b9743d1371d765298d0
tatsumakiroll <paul.shoreman@outlook.com>
2024-04-17 17:14:01 +0100

    GET users, done and tested happy path

--- controllers/api.controllers.js
+++ controllers/api.controllers.js
@@ -1,7 +1,8 @@
-const {selectApi} = require('../models/api.models')
+const { selectApi } = require('../models/api.models')
 
 exports.getApi = (req, res, next) => {
-    return selectApi().then((response) => {
-        res.status(200).send({ availableApi: response })
-    })
+    return selectApi()
+        .then((response) => {
+            res.status(200).send({ availableApi: response })
+        })
 }
