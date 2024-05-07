80588e163c86da468fc80d813230be47719c450f
tatsumakiroll <paul.shoreman@outlook.com>
2024-05-07 09:10:21 +0100

    added cors

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const apiRouter = require('./routes/api-router')
-const {handleCustomErrors, handlePsqlErrors, handleServerErrors} = require('./errors')
+const express = require("express");
+const apiRouter = require("./routes/api-router");
+const {
+  handleCustomErrors,
+  handlePsqlErrors,
+  handleServerErrors,
+} = require("./errors");
 const app = express();
+const cors = require("cors");
 
-app.use(express.json())
+app.use(cors())
+app.use(express.json());
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-app.all('*', (req, res, next) => {
-    res.status(404).send({ message: 'Not found' })
-})
+app.all("*", (req, res, next) => {
+  res.status(404).send({ message: "Not found" });
+});
 
-app.use(handleCustomErrors)
-app.use(handlePsqlErrors)
-app.use(handleServerErrors)
+app.use(handleCustomErrors);
+app.use(handlePsqlErrors);
+app.use(handleServerErrors);
 
-
-module.exports = app
+module.exports = app;
